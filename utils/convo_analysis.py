e8c91f2948b231a8036ac9f8488aa39e5d6df30d
Kiran Bhattacharyya <kiran.bhattacharyya@intusurg.com>
2025-03-14 13:06:23 -0700

    simplify clustering and sample sentence selection

--- utils/convo_analysis.py
+++ utils/convo_analysis.py
@@ -79,13 +79,11 @@ class EmbedSentences:
 
 class ClusterSentences:
 
-    def __init__(self, sentences: list[str], embeddings: list, clustering_method="kmeans", 
-                 optimize: bool=False):
+    def __init__(self, sentences: list[str], embeddings: list, optimize: bool=False):
         self._sentences = sentences
         self.normalize_embeddings(embeddings)
         self.check_length()
         self._optimize = optimize
-        self._clustering_method = clustering_method
     
     def normalize_embeddings(self, embeddings: list) -> None:
         if len(embeddings) > 0:
@@ -99,42 +97,19 @@ class ClusterSentences:
             raise ValueError("Number of sentences and embeddings must be equal")
     
     def run(self) -> dict:
-        if self._clustering_method == "dbscan":
-            cluster_assignments = self.run_dbscan()
-        elif self._clustering_method == "optics":
-            cluster_assignments = self.run_optics()
-        elif self._clustering_method == "kmeans":
-            if not self._optimize:
-                cluster_assignments = self.run_kmeans(num_clusters=np.sqrt(len(self._sentences)))
-            else:
-                max_clusters = min(100, int(1.5*np.sqrt(len(self._sentences))))
-                logging.info(f"Optimizing clustering with max clusters {max_clusters}")
-                num_clusters = self.find_optimal_cluster_number(max_clusters)
-                logging.info(f"Optimized clustering with {num_clusters} clusters")
-                cluster_assignments = self.run_kmeans(num_clusters)
+        if not self._optimize:
+            cluster_assignments = self.run_kmeans(num_clusters=np.sqrt(len(self._sentences)))
         else:
-            raise ValueError(f"Invalid clustering method: {self._clustering_method}")
+            max_clusters = min(100, int(1.5*np.sqrt(len(self._sentences))))
+            logging.info(f"Optimizing clustering with max clusters {max_clusters}")
+            num_clusters = self.find_optimal_cluster_number(max_clusters)
+            logging.info(f"Optimized clustering with {num_clusters} clusters")
+            cluster_assignments = self.run_kmeans(num_clusters)
         clusters = self.assign_sentences_to_clusters(cluster_assignments)
         return clusters
     
     def run_kmeans(self, num_clusters: int=2) -> list[int]:
         return run_kmeans(num_clusters, self._sentences, self._embeddings)
-
-    def run_dbscan(self, min_samples: int=3, eps: float=0.1) -> list[int]:
-        if len(self._sentences) == 0:
-            return []
-        dbscan = DBSCAN(eps=eps, min_samples=min_samples, metric="cosine")
-        dbscan.fit(self._embeddings)
-        cluster_assignments = dbscan.labels_
-        return cluster_assignments
-
-    def run_optics(self, min_samples: int=3, eps: float=0.1) -> list[int]:
-        if len(self._sentences) == 0:
-            return []
-        optics = OPTICS(eps=eps, min_samples=min_samples, metric="cosine")
-        optics.fit(self._embeddings)
-        cluster_assignments = optics.labels_
-        return cluster_assignments
 
     def find_optimal_cluster_number(self, max_clusters: int=10) -> int:
         if len(self._sentences) == 0:
@@ -194,24 +169,18 @@ def summarize_sentences(sentences: str, product_description: str,
     SENTENCES:
     {sentences}
     
-    Provide only the theme, a short description, and some sample sentences from the list above that help
+    Provide only the theme, a short description, and 3-5 sample sentences from the list above that help
     support your choice of the theme and the description. Here is the response format
-    
+
     Theme: [theme]
     Description: [description]
-    Sample Sentences: [sample_sentences]
+    Sample Sentences: 
+    1. [sample_sentence]
+    2. [sample_sentence]
+    ...
     """
     output = call_llm(prompt, api_key)
     theme, rest = output.split("Description: ")
     description, sample_sentences = rest.split("Sample Sentences: ")
     theme = theme.split("Theme")[-1].strip()
     return theme, description, sample_sentences
-
-def find_all_representative_sentences(self, number: int, summaries: dict, clusters: dict) -> dict:
-    return None  # TODO: Implement this function to find the most representative sentences from the cluster summaries
-
-def find_representative_sentences(self, number: int, summary_text: str, sentences: list[str]) -> list[str]:
-    return None  # TODO: Implement this function to find the most representative sentences from the cluster summary
-
-
-    
